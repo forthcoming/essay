a078fbd4b75d4dd2db680221ab722d6f6d286531
neos <akatsuki10086@gmail.com>
2018-09-07 11:31:52 +0800

    Update sds.c

--- common/sds.c
+++ common/sds.c
@@ -443,6 +443,18 @@ sds sdscatlen(sds s, const void *t, size_t len) {
     return s;
 }
 
+int sdscmp(const sds s1, const sds s2) {
+    size_t l1, l2, minlen;
+    int cmp;
+
+    l1 = sdslen(s1);
+    l2 = sdslen(s2);
+    minlen = (l1 < l2) ? l1 : l2;
+    cmp = memcmp(s1,s2,minlen);
+    if (cmp == 0) return l1>l2? 1: (l1<l2? -1: 0);
+    return cmp;
+}
+
 int main()
 {
     // 内存操作函数单位是1byte即8bit
