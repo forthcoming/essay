a08cc673ff747a01f0c3310e01058a92a8a611cd
neos <akatsuki10086@gmail.com>
2019-02-15 10:54:55 +0800

    Update shell.sh

--- linux/shell.sh
+++ linux/shell.sh
@@ -11,7 +11,7 @@ ctrl U 在提示符下,将整行命令删除
 ctrl R 在history中搜索匹配命令
 ctrl L 等价于clear
 ctrl insert 复制
-shift insert 复制
+shift insert 粘贴
 ctrl shift T   打开一个新的终端窗口
 
 tar(打包压缩目录或文件,压缩后文件格式为*.tar.gz)
@@ -31,7 +31,7 @@ mysql -uu_coolshot -P12450 -h10.16.2.46 -p  # 外部连接mysql
 
 ps & netstat
 ps -ef | grep httpd # 查看httpd进程,第二列显示的是进程ID,可配合kill命令来关闭进程,dos下用tasklist | findstr httpd.exe
-常用参数：
+常用参数:
 -ef: 标准格式输出,注意"-"不能省
 aux: BSD格式输出
 netstat -apn | grep 27017 # 查看27017端口使用情况(包括进程名,pid,port等信息),dos下用-abn
@@ -49,7 +49,7 @@ P: 按CPU占有率降序排列
 N: 以PID大小降序排列
 q: 退出top模式
 c: 开启COMMAND列详情,可以找到对应的进程启动者
-空格:手动刷新
+空格: 手动刷新
 
 nohup &
 &: 后台运行,即使你用ctrl C,进程照样运行,因为对SIGINT信号免疫.但如果你直接关掉shell,进程同样消失.可见&对SIGHUP信号不免疫
@@ -57,12 +57,12 @@ nohup: 忽略SIGHUP信号,关闭shell,进程还是存在的.但如果你直接�
 所以要让进程真正不受Ctrl C和shell关闭的影响,就同时用nohup processname &,nohup命令使标准输出和标准错误默认重定向到nohup.out文件
 nohup java -jar monitor.jar > /home/java.log &
 
-合并文件(uniq命令可以去除排序过的文件中的重复行,也就是说为了使uniq起作用，所有的重复行必须是相邻的)
+合并文件(uniq命令可以去除排序过的文件中的重复行,也就是说为了使uniq起作用,所有的重复行必须是相邻的)
 取出两个文件行的并集(重复的行只保留一份),结果按行排序
 cat file1 file2 | sort | uniq
-取出两个文件行的交集(只留下同时存在于两个文件的行)，结果按行排序
+取出两个文件行的交集(只留下同时存在于两个文件的行),结果按行排序
 cat file1 file2 | sort | uniq -d
-合并两个文件，删除行交集，留下其他的行，结果按行排序
+合并两个文件,删除行交集,留下其他的行,结果按行排序
 cat file1 file2 | sort | uniq -u
 
 date
@@ -74,7 +74,7 @@ Thu Dec 15 16:55:22 CST 2016
 common
 du -s * | sort -n | tail  # 列出当前目录里最大的10个文件
 ps aux | sort -nk +4 | tail  #  列出头十个最耗内存的进程
-tail -f file.log | sed '/^Finished: SUCCESS$/ q'  # 当file.log里出现Finished: SUCCESS时候就退出tail，用于实时监控并过滤log是否出现了某条记录
+tail -f file.log | sed '/^Finished: SUCCESS$/ q'  # 当file.log里出现Finished: SUCCESS时候就退出tail,用于实时监控并过滤log是否出现了某条记录
 history | awk '{CMD[$2]++;count++;} END { for (a in CMD )print CMD[a] " " CMD[a]/count*100 "% " a }' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n10  # 输出最常用的十条命令
 lsof -i:80   #  列出80端口现在运行什么程序
 uname -a     #  -n: 显示主机名 -r: 显示内核版本 -a: 显示所有信息
