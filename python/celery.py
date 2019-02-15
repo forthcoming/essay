1b579bbff8d39ef15bf7aa15f6ef9db454668f4c
neos <akatsuki10086@gmail.com>
2019-02-15 11:14:48 +0800

    Update celery.py

--- python/celery.py
+++ python/celery.py
@@ -66,8 +66,9 @@ celery -A Ticktock worker -l info -B --logfile=/root/Desktop/Ticktock/%n.log    
 ### 调用
 from Ticktock.celery import * 
 result=test.delay(20)
+# celery的broker,backend,task,调用task的项目,都可以在不同机器上
 # result=app.send_task('tobedone',[3,4],{})  # 任务名错了不报错,字典传param=param型参数,适于远程调用
 print(result.get(timeout=4))
 print(result.id)
 print(result.ready())  # returns whether the task has finished processing or not
-#print(result.traceback)
+# print(result.traceback)
