6c828c992daf718e510d5e9b30545b4faaeb1bd6
neos <akatsuki10086@gmail.com>
2019-03-08 19:05:29 +0800

    Update shell.sh

--- linux/shell.sh
+++ linux/shell.sh
@@ -124,6 +124,7 @@ cd - #返回到上个目录
 wc a.txt  #行数  单词数  字节数  文件名, ls | wc  
 last -n   # 显示最近n次用户登录信息
 如果shell脚本出现找不到路径或其他莫名错误,先用vim打开脚本,:set ff查看文件格式,如果是dos,应该为:set ff=unix
+curl ifconfig.me # 查看服务器公网IP,还可以通过curl cip.cc,ifconfig等方式获取
 
 Ubuntu
 launcher地址: /usr/share/applications/eclipse.desktop
