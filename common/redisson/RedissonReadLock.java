d0b6ba1b6cc66377fe0b14cf200cb88429e30912
neos <akatsuki10086@gmail.com>
2019-03-24 00:24:01 +0800

    Update RedissonReadLock.java

--- common/redisson/RedissonReadLock.java
+++ common/redisson/RedissonReadLock.java
@@ -1,49 +1,4 @@
-/**
- * Copyright (c) 2013-2019 Nikita Koksharov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.redisson;
-
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-
-import org.redisson.api.RFuture;
-import org.redisson.api.RLock;
-import org.redisson.client.codec.LongCodec;
-import org.redisson.client.codec.StringCodec;
-import org.redisson.client.protocol.RedisCommands;
-import org.redisson.client.protocol.RedisStrictCommand;
-import org.redisson.command.CommandAsyncExecutor;
-import org.redisson.pubsub.LockPubSub;
-
-/**
- * Lock will be removed automatically if client disconnects.
- *
- * @author Nikita Koksharov
- *
- */
 public class RedissonReadLock extends RedissonLock implements RLock {
-
-    public RedissonReadLock(CommandAsyncExecutor commandExecutor, String name) {
-        super(commandExecutor, name);
-    }
-
-    @Override
-    String getChannelName() {
-        return prefixName("redisson_rwlock", getName());
-    }
     
     String getWriteLockName(long threadId) {
         return super.getLockName(threadId) + ":write";
@@ -53,6 +8,27 @@ public class RedissonReadLock extends RedissonLock implements RLock {
         return suffixName(getName(), getLockName(threadId)) + ":rwlock_timeout"; 
     }
     
+    '''
+    tryLockInnerAsync方法是读锁加锁的最终方法
+    KEYS[1]: 锁在redis中的key
+    KEYS[2]: 超时名称的前缀
+    ARGV[1]: 锁超时时间
+    ARGV[2]: 锁的名称
+    ARGV[3]: 写锁的名称,它是在锁名称的后面加上write
+    加锁流程如下:
+    1. 获取锁的mode,如果锁的mode为false,表示之前没有设置过读写锁,此时可以获得读锁.
+        1. 将锁的mode设置为read
+        2. 将锁名称对应的线程数设置为1
+        3. 设置超时名称
+        4. 设置超时名称的过期时间
+        5. 设置锁的过期时间
+    2. 如果锁的mode为read或者mode为write并且持有写锁的线程为当前线程,此时可以继续加读锁.换句话说,如果当前存在读锁或者持有写锁的是当前线程,都可以加读锁。
+        1. 将锁名称对应的线程数增加1
+        2. 设置超时名称
+        3. 设置超时名称的过期时间
+        4. 设置锁的过期时间
+    3. 否则返回当前锁的过期时间
+    '''
     @Override
     <T> RFuture<T> tryLockInnerAsync(long leaseTime, TimeUnit unit, long threadId, RedisStrictCommand<T> command) {
         internalLockLeaseTime = unit.toMillis(leaseTime);
@@ -79,7 +55,24 @@ public class RedissonReadLock extends RedissonLock implements RLock {
                         Arrays.<Object>asList(getName(), getReadWriteTimeoutNamePrefix(threadId)), 
                         internalLockLeaseTime, getLockName(threadId), getWriteLockName(threadId));
     }
-
+    
+    '''
+    unlockInnerAsync方法是读锁解锁的最终方法
+    KEYS[1]: 锁在redis中的key
+    KEYS[2]: channel名称,用于发送解锁的消息
+    KEYS[3]: 超时名称的前缀
+    KEYS[4]: key的前缀
+    ARGV[1]: 解锁消息
+    ARGV[2]: 锁的名称
+    解锁流程如下: 
+    1. 获取锁的mode,如果锁的mode为false,表示之前没有设置过读写锁,此时可以直接解锁,发送解锁消息,返回1
+    2. 查看锁是否存在,如果不存在锁返回nil
+    3. 将锁名称对应的线程数减1,如果剩余的线程数为0,表示没有其他线程持有该锁了,删除该锁
+    4. 删除超时名称
+    5. 如果当前锁结构对应的hash表大小大于1,表示有其他线程持有锁,遍历其中里面所有锁的超时时间,将最大的超时时间(maxRemainTime)作为整个锁结构的超时时间
+       如果最大的超时时间(maxRemainTime)大于0,表示还有其他线程持有锁,不能完全释放锁,返回0,如果锁的mode为write返回0,不能释放写锁.
+    6. 否则没有其他线程持有锁,此时可以彻底释放锁,删除锁结构,发送解锁消息,返回1
+    '''
     @Override
     protected RFuture<Boolean> unlockInnerAsync(long threadId) {
         String timeoutPrefix = getReadWriteTimeoutNamePrefix(threadId);
@@ -132,10 +125,6 @@ public class RedissonReadLock extends RedissonLock implements RLock {
                 LockPubSub.UNLOCK_MESSAGE, getLockName(threadId));
     }
 
-    protected String getKeyPrefix(long threadId, String timeoutPrefix) {
-        return timeoutPrefix.split(":" + getLockName(threadId))[0];
-    }
-    
     @Override
     protected RFuture<Boolean> renewExpirationAsync(long threadId) {
         String timeoutPrefix = getReadWriteTimeoutNamePrefix(threadId);
@@ -165,10 +154,6 @@ public class RedissonReadLock extends RedissonLock implements RLock {
             internalLockLeaseTime, getLockName(threadId));
     }
     
-    @Override
-    public Condition newCondition() {
-        throw new UnsupportedOperationException();
-    }
 
     @Override
     public RFuture<Boolean> forceUnlockAsync() {
@@ -181,13 +166,6 @@ public class RedissonReadLock extends RedissonLock implements RLock {
                 "end; " +
                 "return 0; ",
                 Arrays.<Object>asList(getName(), getChannelName()), LockPubSub.UNLOCK_MESSAGE);
-    }
-
-    @Override
-    public boolean isLocked() {
-        RFuture<String> future = commandExecutor.writeAsync(getName(), StringCodec.INSTANCE, RedisCommands.HGET, getName(), "mode");
-        String res = get(future);
-        return "read".equals(res);
     }
 
 }
