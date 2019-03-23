5f98cf6c5f1d45684fc1830a4f158e30bee63435
neos <akatsuki10086@gmail.com>
2019-03-24 00:31:19 +0800

    Update RedissonWriteLock.java

--- common/redisson/RedissonWriteLock.java
+++ common/redisson/RedissonWriteLock.java
@@ -1,55 +1,25 @@
-/**
- * Copyright (c) 2013-2019 Nikita Koksharov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.redisson;
-
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-
-import org.redisson.api.RFuture;
-import org.redisson.api.RLock;
-import org.redisson.client.codec.LongCodec;
-import org.redisson.client.codec.StringCodec;
-import org.redisson.client.protocol.RedisCommands;
-import org.redisson.client.protocol.RedisStrictCommand;
-import org.redisson.command.CommandAsyncExecutor;
-import org.redisson.pubsub.LockPubSub;
-
-/**
- * Lock will be removed automatically if client disconnects.
- *
- * @author Nikita Koksharov
- *
- */
 public class RedissonWriteLock extends RedissonLock implements RLock {
-
-    protected RedissonWriteLock(CommandAsyncExecutor commandExecutor, String name) {
-        super(commandExecutor, name);
-    }
-
-    @Override
-    String getChannelName() {
-        return prefixName("redisson_rwlock", getName());
-    }
 
     @Override
     protected String getLockName(long threadId) {
         return super.getLockName(threadId) + ":write";
     }
     
+    '''
+    tryLockInnerAsync方法是写锁加锁的最终方法
+    KEYS[1]: 锁在redis中的key
+    ARGV[1]: 锁的超时时间
+    ARGV[2]: 锁的名称
+    加锁流程如下: 
+    1. 获取锁的mode,如果锁的mode为false,表示之前没有设置过读写锁,此时可以获得写锁.
+        1. 将锁的mode设置为write
+        2. 将锁名称对应的线程数设置为1
+        3. 设置锁的过期时间
+    2. 如果锁的mode为write,并且持有写锁的线程为当前线程,此时可以继续加写锁
+        1. 将锁名称对应的线程数增加1
+        2. 增加锁的过期时间
+    3. 否则返回当前锁的过期时间
+    '''
     @Override
     <T> RFuture<T> tryLockInnerAsync(long leaseTime, TimeUnit unit, long threadId, RedisStrictCommand<T> command) {
         internalLockLeaseTime = unit.toMillis(leaseTime);
@@ -74,7 +44,23 @@ public class RedissonWriteLock extends RedissonLock implements RLock {
                         Arrays.<Object>asList(getName()), 
                         internalLockLeaseTime, getLockName(threadId));
     }
-
+    
+    '''
+    unlockInnerAsync方法是写锁解锁的最终方法
+    KEYS[1]: 锁在redis中的key
+    KEYS[2]: channel名称，用于发送解锁的消息
+    ARGV[1]: 解锁消息
+    ARGV[2]: 锁的过期时间
+    ARGV[3]: 锁的名称
+    解锁流程如下:
+    1. 获取锁的mode,如果锁的mode为false,表示之前没有设置过读写锁,此时可以直接解锁,发送解锁消息,返回1
+    2. 如果锁的mode为write
+        1. 检查锁是否存在,如果不存在则返回nil
+        2. 将锁名称对应的线程数减1,如果剩余的线程数大于0,表示还有其他线程持有该锁,重新设置锁结构的过期时间
+        3. 如果剩余的线程数为0,表示没有其他线程持有该写锁了,于是删除该锁,返回1.
+           如果当前锁结构对应的hash表大小等于1,表示没有其他线程持有锁,此时可以彻底释放锁,删除锁结构,发送解锁消息,
+           否则表示还存在读锁,于是将锁的mode设置为read
+    '''
     @Override
     protected RFuture<Boolean> unlockInnerAsync(long threadId) {
         return commandExecutor.evalWriteAsync(getName(), LongCodec.INSTANCE, RedisCommands.EVAL_BOOLEAN,
@@ -109,11 +95,6 @@ public class RedissonWriteLock extends RedissonLock implements RLock {
         Arrays.<Object>asList(getName(), getChannelName()), 
         LockPubSub.READ_UNLOCK_MESSAGE, internalLockLeaseTime, getLockName(threadId));
     }
-    
-    @Override
-    public Condition newCondition() {
-        throw new UnsupportedOperationException();
-    }
 
     @Override
     public RFuture<Boolean> forceUnlockAsync() {
@@ -126,13 +107,6 @@ public class RedissonWriteLock extends RedissonLock implements RLock {
               "end; " +
               "return 0; ",
               Arrays.<Object>asList(getName(), getChannelName()), LockPubSub.READ_UNLOCK_MESSAGE);
-    }
-
-    @Override
-    public boolean isLocked() {
-        RFuture<String> future = commandExecutor.writeAsync(getName(), StringCodec.INSTANCE, RedisCommands.HGET, getName(), "mode");
-        String res = get(future);
-        return "write".equals(res);
     }
 
 }
