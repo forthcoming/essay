47d669e5f51cd3ac3c5d7bf718cccab80432914f
neos <akatsuki10086@gmail.com>
2019-03-25 10:49:12 +0800

    Update RedissonLock.java

--- common/redisson/RedissonLock.java
+++ common/redisson/RedissonLock.java
@@ -15,6 +15,11 @@ public class RedissonLock extends RedissonExpirable implements RLock {
             internalLockLeaseTime, getLockName(threadId));
     }
 
+    '''
+    KEYS[1]: 需要加锁的key,字符串类型
+    ARGV[1]: 锁的超时时间,防止死锁
+    ARGV[2]: 锁的唯一标志,uuid:threadId
+    '''
     <T> RFuture<T> tryLockInnerAsync(long leaseTime, TimeUnit unit, long threadId, RedisStrictCommand<T> command) {
         internalLockLeaseTime = unit.toMillis(leaseTime);
 
@@ -46,6 +51,13 @@ public class RedissonLock extends RedissonExpirable implements RLock {
                 Arrays.<Object>asList(getName(), getChannelName()), LockPubSub.UNLOCK_MESSAGE);
     }
 
+    '''
+    KEYS[1]: 需要加锁的key,字符串类型
+    KEYS[2]: redis消息的ChannelName,一个分布式锁对应唯一一个ChannelName:"redisson_lock_channel_{"+getName()+"}"
+    ARGV[1]: redis消息体,只需要一个字节标记,主要标记key已经解锁,再结合redis的subscribe,能唤醒其他订阅解锁消息的客户端线程申请锁
+    ARGV[2]: 锁的超时时间,防止死锁
+    ARGV[3]: 锁的唯一标志,uuid:threadId
+    '''
     protected RFuture<Boolean> unlockInnerAsync(long threadId) {
         return commandExecutor.evalWriteAsync(getName(), LongCodec.INSTANCE, RedisCommands.EVAL_BOOLEAN,
                 "if (redis.call('exists', KEYS[1]) == 0) then " +
