ae5980b2320ecae7285055f6cad2ff94cab44b1f
neos <akatsuki10086@gmail.com>
2019-04-21 23:13:47 +0800

    Update mysql.sql

--- db/mysql.sql
+++ db/mysql.sql
@@ -9,7 +9,8 @@ mysql -h192.168.9.6 -uroot -p
 创建用户/密码,并赋予该用户可以用192.168.1网段登陆:
 GRANT ALL PRIVILEGES ON *.* TO 'avatar'@'192.168.1.%' IDENTIFIED BY 'avatar' WITH GRANT OPTION;
 flush privileges;
-注意:以上所有改变都保存在mysql库下的user表中
+注意:
+以上所有改变都保存在mysql库下的user表中
 select host,user,authentication_string from mysql.user;
 
 
@@ -22,7 +23,6 @@ auto_increment:there can be only one auto column and it must be defined as a key
 ? int     //显示int的具体属性
 ? show    //显示show的语法
 \s        //查看当前用户的信息
-从服务器执行查询操作,降低主服务器访问压(实时性要求高的数据仍需要从主数据查询)
 mysql -u[username] -p[password] -h[host] -P[port]
 create database [dname];
 create table t_name as select * from t1_name;  // 不完全复制表结构(只包含基本的字段信息),并插入数据
@@ -69,7 +69,7 @@ reset master;       # 清空所有binlog日志
 show binlog events in '201810-08571-bin.000001' from pos limit m,n;  # 日志查询
 
 
-主从复制
+主从复制(slave执行查询操作,降低master访问压力,实时性要求高的数据仍需要从master查询)
 1. 主开启binlog
 2. 主从设置唯一的server_id
 3. 主创建主从复制用户(repl)并授权
@@ -284,14 +284,15 @@ Myisam & InnoDB(默认)
 区别:
 1. InnoDB支持事务,Myisam不支持,对于InnoDB每一条SQL语言都默认封装成事务,自动提交,这样会影响速度,所以最好把多条SQL语言放在begin和commit之间,组成一个事务
 2. InnoDB支持外键,而Myisam不支持,对一个包含外键的InnoDB表转为MYISAM会失败
-3. InnoDB是聚集索引,必须要有主键,通过主键索引效率很高,但辅助索引需要两次查询,先查询到主键,然后再通过主键查询到数据。因此主键不应该过大,因为主键太大,其他索引也都会很大;Myisam是非聚集索引,数据文件是分离的,索引保存的是数据文件的指针。主键索引和辅助索引是独立的。
+3. InnoDB是聚集索引,必须要有主键,通过主键索引效率很高,但辅助索引需要两次查询,先查询到主键,然后再通过主键查询到数据.因此主键不应该过大,因为主键太大,其他索引也都会很大
+   Myisam是非聚集索引,数据文件是分离的,索引保存的是数据文件的指针,主键索引和辅助索引是独立的
 4. InnoDB不保存表的具体行数,执行select count(*) from table时需要全表扫描,而MyISAM用一个变量保存了整个表的行数,执行上述语句时只需要读出该变量即可,速度很快
-5. InnoDB只包含一个*.frm文件,MyISAM包含*.frm(结构) , *.MYD(数据文件) ,*.MYI(索引)
+5. InnoDB只包含一个*.frm文件,MyISAM包含*.frm(结构), *.MYD(数据文件),*.MYI(索引)
 如何选择:
 1. 是否要支持事务,如果要请选择innodb,如果不需要可以考虑Myisam
 2. 如果表中绝大多数都只是读查询,可以考虑Myisam,如果既有读写也挺频繁,请使用InnoDB
 3. 系统奔溃后,Myisam恢复起来更困难,能否接受
-4. MySQL5.5版本开始Innodb已经成为Mysql的默认引擎(之前是Myisam),说明其优势是有目共睹
+4. MySQL5.5版本开始Innodb已经成为Mysql的默认引擎(之前是Myisam)
 
 
 Oracle批量删除和查询表数据
