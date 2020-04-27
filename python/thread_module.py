5a3f181af63433d4d517522dc0f130d23c667ba8
neos <akatsuki10086@gmail.com>
2020-04-28 00:59:21 +0800

    Update thread_module.py

--- python/thread_module.py
+++ python/thread_module.py
@@ -412,3 +412,26 @@ class Barrier:
             else:
                 self._state = 0
             self._cond.notify_all()
+
+            
+class Timer(Thread):  # Call a function after a specified number of seconds
+    # t = Timer(30.0, f, args=None, kwargs=None)
+    # t.start()
+
+    def __init__(self, interval, function, args=None, kwargs=None):
+        Thread.__init__(self)
+        self.interval = interval
+        self.function = function
+        self.args = args if args is not None else []
+        self.kwargs = kwargs if kwargs is not None else {}
+        self.finished = Event()
+
+    def cancel(self):   # Stop the timer if it hasn't finished yet.
+        self.finished.set()
+
+    def run(self):
+        self.finished.wait(self.interval)
+        if not self.finished.is_set():
+            self.function(*self.args, **self.kwargs)
+        self.finished.set()
+
