1454ed93dbbccc2a14d46c145b35c8efda80a1c3
neos <akatsuki10086@gmail.com>
2020-09-20 20:38:31 +0800

    Update python.py

--- python/python.py
+++ python/python.py
@@ -1074,7 +1074,7 @@ print(d)   # {'d': None, 'b': 2, 'c': 4, 'e': 'avatar'}
 
 one = {'a':1,'b':2}
 two = {'a':3,'c':2}
-three = {**one,**two}   # {'a': 3, 'b': 2, 'c': 2},合并两个字典
+three = one | two  # {'a': 3, 'b': 2, 'c': 2},合并两个字典
                                        
 # dict.setdefault(key, default=None)
 # key -- 查找的键值
