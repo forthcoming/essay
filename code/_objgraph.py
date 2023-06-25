96af26174babcf1952d46b43e506d587a55883ec
neos <akatsuki10086@gmail.com>
2023-06-25 20:50:01 +0800

    Update _objgraph.py

--- code/_objgraph.py
+++ code/_objgraph.py
@@ -18,24 +18,6 @@ def _long_typename(obj):
         return name
 
 
-def get_leaking_objects():
-    """
-    Return objects that do not have any referents.
-    These could indicate reference-counting bugs in C code.  Or they could be legitimate.
-    Note that the GC does not track simple objects like int or str.
-    """
-    gc.collect()
-    objects = gc.get_objects()
-    try:
-        ids = {id(i) for i in objects}
-        for i in objects:
-            # Return a list of objects directly referred to by any of the arguments
-            ids -= {id(j) for j in gc.get_referents(i)}
-        return [i for i in objects if id(i) in ids]  # this then is our set of objects without referrers
-    finally:
-        del objects, i  # clear cyclic references to frame
-
-
 def count(typename):
     """
     Count objects tracked by the garbage collector with a given class name.
@@ -60,6 +42,49 @@ def count(typename):
             return sum(1 for o in objects if _short_typename(o) == typename)
     finally:
         del objects  # clear cyclic references to frame
+
+
+def show_growth(limit=10, shortnames=True, _filter=None, peak_stats={}):
+    """
+    Show the increase in peak object counts since last call.
+    peak_stats, a dictionary from type names to previously seen peak object counts.
+    Usually you don't need to pay attention to this argument.
+    """
+    gc.collect()  # 手动执行垃圾回收,避免循环引用干扰
+    stats = type_stats(shortnames=shortnames, _filter=_filter)
+    deltas = {}
+    for name, count in stats:
+        old_count = peak_stats.get(name, 0)
+        if count > old_count:
+            deltas[name] = count - old_count
+            peak_stats[name] = count
+    deltas = sorted(deltas.items(), key=operator.itemgetter(1), reverse=True)
+    if limit:
+        deltas = deltas[:limit]
+    result = [(name, stats[name], delta) for name, delta in deltas]
+    width = max(len(name) for name, _, _ in result)
+    for name, count, delta in result:
+        sys.stdout.write('%-*s%9d %+9d\n' % (width, name, count, delta))
+
+
+def show_most_common_types(limit=10, shortnames=True, _filter=None):
+    """
+    Print the table of types of most common instances.
+    If ``filter`` is specified, it should be a function taking one argument and returning a boolean. Objects for which ``filter(obj)`` returns ``False`` will be ignored.
+    Example:
+        >>> show_most_common_types(limit=5)
+        tuple                      8959
+        function                   2442
+        wrapper_descriptor         1048
+        dict                       953
+        builtin_function_or_method 800
+    """
+    stats = sorted(type_stats(shortnames=shortnames, _filter=_filter).items(), key=operator.itemgetter(1), reverse=True)
+    if limit:
+        stats = stats[:limit]
+    width = max(len(name) for name, count in stats)
+    for name, count in stats:
+        sys.stdout.write('%-*s %i\n' % (width, name, count))
 
 
 def type_stats(shortnames=True, _filter=None):
@@ -89,46 +114,3 @@ def type_stats(shortnames=True, _filter=None):
         return stats
     finally:
         del objects  # clear cyclic references to frame
-
-
-def show_most_common_types(limit=10, shortnames=True, _filter=None):
-    """
-    Print the table of types of most common instances.
-    If ``filter`` is specified, it should be a function taking one argument and returning a boolean. Objects for which ``filter(obj)`` returns ``False`` will be ignored.
-    Example:
-        >>> show_most_common_types(limit=5)
-        tuple                      8959
-        function                   2442
-        wrapper_descriptor         1048
-        dict                       953
-        builtin_function_or_method 800
-    """
-    stats = sorted(type_stats(shortnames=shortnames, _filter=_filter).items(), key=operator.itemgetter(1), reverse=True)
-    if limit:
-        stats = stats[:limit]
-    width = max(len(name) for name, count in stats)
-    for name, count in stats:
-        sys.stdout.write('%-*s %i\n' % (width, name, count))
-
-
-def show_growth(limit=10, shortnames=True, _filter=None, peak_stats={}):
-    """
-    Show the increase in peak object counts since last call.
-    peak_stats, a dictionary from type names to previously seen peak object counts.
-    Usually you don't need to pay attention to this argument.
-    """
-    gc.collect()  # 手动执行垃圾回收,避免循环引用干扰
-    stats = type_stats(shortnames=shortnames, _filter=_filter)
-    deltas = {}
-    for name, count in stats:
-        old_count = peak_stats.get(name, 0)
-        if count > old_count:
-            deltas[name] = count - old_count
-            peak_stats[name] = count
-    deltas = sorted(deltas.items(), key=operator.itemgetter(1), reverse=True)
-    if limit:
-        deltas = deltas[:limit]
-    result = [(name, stats[name], delta) for name, delta in deltas]
-    width = max(len(name) for name, _, _ in result)
-    for name, count, delta in result:
-        sys.stdout.write('%-*s%9d %+9d\n' % (width, name, count, delta))
