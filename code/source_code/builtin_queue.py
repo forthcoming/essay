5819e7adb8a205bf545f60b8952d95495ff7584d
neos <akatsuki10086@gmail.com>
2023-07-04 17:26:49 +0800

    Update builtin_queue.py

---
+++ code/source_code/builtin_queue.py
@@ -0,0 +1,182 @@
+import threading
+from collections import deque
+from heapq import heappush, heappop
+from time import monotonic as time
+
+# 参考python3.11的queue.py
+
+
+__all__ = ['Empty', 'Full', 'Queue', 'PriorityQueue', 'LifoQueue', 'SimpleQueue']
+
+
+class Empty(Exception):  # Exception raised by Queue.get(block=0)/get_nowait().
+    pass
+
+
+class Full(Exception):  # Exception raised by Queue.put(block=0)/put_nowait().
+    pass
+
+
+class Queue:
+
+    def __init__(self, maxsize=0):
+        self.maxsize = maxsize
+        self._init(maxsize)
+        self.mutex = threading.Lock()  # 当队列发生变化时,必须保持互斥锁,mutex在三个conditions之间共享,因此获取和释放conditions也会获取和释放mutex
+        # not_empty与not_full拥有不同的_waiters,目的是把因put挂起的线程和因get挂起的线程区分开
+        # Notify not_empty whenever an item is added to the queue; a thread waiting to get is notified then.
+        # Notify not_full whenever an item is removed from the queue; a thread waiting to put is notified then.
+        self.not_empty = threading.Condition(self.mutex)
+        self.not_full = threading.Condition(self.mutex)
+        self.all_tasks_done = threading.Condition(self.mutex)  # 每当未完成的任务数量降至零时通知all_tasks_done,等待join()的线程被通知恢复
+        self.unfinished_tasks = 0
+
+    def qsize(self):  # Return the approximate size of the queue (not reliable!).
+        with self.mutex:
+            return self._qsize()
+
+    def task_done(self):  # 要放到任务执行完的最后一步调用,标记本次任务结束
+        """
+        Indicate that a formerly enqueued task is complete.Used by Queue consumer threads.
+        For each get() used to fetch a task, a subsequent call to task_done() tells the queue that the processing on the task is complete.
+        If a join() is currently blocking, it will resume when all items have been processed
+        meaning that a task_done() call was received for every item that had been put() into the queue.
+        Raises a ValueError if called more times than there were items placed in the queue.
+        """
+        with self.all_tasks_done:
+            unfinished = self.unfinished_tasks - 1
+            if unfinished <= 0:
+                if unfinished < 0:
+                    raise ValueError('task_done() called too many times')
+                self.all_tasks_done.notify_all()
+            self.unfinished_tasks = unfinished
+
+    def join(self):  # 使用前要保证所有生产者都已完成生产任务(可通过线程join达到目的)
+        """
+        Blocks until all items in the Queue have been gotten and processed.
+        The count of unfinished tasks goes up whenever an item is added to the queue.
+        The count goes down whenever a consumer thread calls task_done() to indicate the item was retrieved
+        and all work on it is complete. When the count of unfinished tasks drops to zero, join() unblocks.
+        """
+        with self.all_tasks_done:
+            while self.unfinished_tasks:
+                self.all_tasks_done.wait()
+
+    def put(self, item, block=True, timeout=None):
+        """
+        If optional args 'block' is true and 'timeout' is None (the default), block if necessary
+        until a free slot is available.If 'timeout' is a non-negative number, it blocks at most 'timeout' seconds
+        and raises the Full exception if no free slot was available within that time,Otherwise ('block' is false),
+        put an item on the queue if a free slot is immediately available, else raise the Full exception(忽略timeout)
+        """
+        with self.not_full:  # 与with self.not_empty,with self.all_tasks_done,with self.mutex等价
+            if self.maxsize > 0:
+                if not block:
+                    if self._qsize() >= self.maxsize:
+                        raise Full
+                elif timeout is None:
+                    while self._qsize() >= self.maxsize:
+                        self.not_full.wait()
+                elif timeout < 0:
+                    raise ValueError("'timeout' must be a non-negative number")
+                else:
+                    end_time = time() + timeout
+                    while self._qsize() >= self.maxsize:
+                        remaining = end_time - time()
+                        if remaining <= 0.0:
+                            raise Full
+                        self.not_full.wait(remaining)
+            self._put(item)
+            self.unfinished_tasks += 1
+            self.not_empty.notify()
+
+    def get(self, block=True, timeout=None):
+        """
+        If optional args 'block' is true and 'timeout' is None (the default),block if necessary until an item is available.
+        If 'timeout' is a non-negative number, it blocks at most 'timeout' seconds
+        and raises the Empty exception if no item was available within that time,Otherwise ('block' is false),
+        return an item if one is immediately available, else raise the Empty exception(忽略timeout)
+        """
+        with self.not_empty:
+            if not block:
+                if not self._qsize():
+                    raise Empty
+            elif timeout is None:
+                while not self._qsize():
+                    self.not_empty.wait()
+            elif timeout < 0:
+                raise ValueError("'timeout' must be a non-negative number")
+            else:
+                end_time = time() + timeout
+                while not self._qsize():
+                    remaining = end_time - time()
+                    if remaining <= 0.0:
+                        raise Empty
+                    self.not_empty.wait(remaining)
+            item = self._get()
+            self.not_full.notify()
+            return item
+
+    # Override these methods to implement other queue organizations (e.g. stack or priority queue).
+    # These will only be called with appropriate locks held
+    def _init(self, maxsize):
+        self.queue = deque()
+
+    def _qsize(self):
+        return len(self.queue)
+
+    # Put a new item in the queue
+    def _put(self, item):
+        self.queue.append(item)
+
+    # Get an item from the queue
+    def _get(self):
+        return self.queue.popleft()
+
+
+class PriorityQueue(Queue):
+
+    def _init(self, maxsize):
+        self.queue = []
+
+    def _put(self, item):
+        heappush(self.queue, item)
+
+    def _get(self):
+        return heappop(self.queue)
+
+
+class LifoQueue(Queue):
+
+    def _init(self, maxsize):
+        self.queue = []
+
+    def _get(self):
+        return self.queue.pop()
+
+
+class SimpleQueue:
+    """
+    缺少如任务追踪等高级功能的简单队列
+    Note: This pure Python implementation is not reentrant.while this pure Python version provides fairness
+    by using a threading.Semaphore which is itself fair,being based on threading.Condition,
+    fairness is not part of the API contract.This allows the C version to use a different implementation.
+    """
+
+    def __init__(self):
+        self._queue = deque()
+        self._count = threading.Semaphore(0)
+
+    def put(self, item):
+        self._queue.append(item)
+        self._count.release()
+
+    def get(self, block=True, timeout=None):
+        if timeout is not None and timeout < 0:
+            raise ValueError("'timeout' must be a non-negative number")
+        if not self._count.acquire(block, timeout):
+            raise Empty
+        return self._queue.popleft()
+
+    def qsize(self):
+        return len(self._queue)
