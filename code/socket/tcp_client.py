62922c5f1b85c12fdd052aaa84428e6f3e9da21a
neos <akatsuki10086@gmail.com>
2023-07-20 18:25:17 +0800

    update

--- code/socket/tcp_client.py
+++ code/socket/tcp_client.py
@@ -1,11 +1,40 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 9999))
 
-print(client.recv(1024))  # 接收欢迎消息
-for data in [b'Michael', b'Tracy', b'Sarah']:
-    client.send(data)
-    print(client.recv(1024))
-client.send(b'exit')
-client.close()
+def tcp_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 9999))
+    print(client.recv(1024))  # 接收欢迎消息
+    for data in [b'Michael', b'Tracy', b'Sarah']:
+        client.send(data)
+        print(client.recv(1024))
+    client.send(b'exit')
+    client.close()
+
+
+def http_request():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET指定使用IPv4协议,SOCK_STREAM指定使用面向流的TCP协议
+    client.connect(('www.sina.com.cn', 80))  # 参数类型是tuple
+
+    # TCP连接创建的是双向通道,双方都可以同时给对方发数据,但是谁先发谁后发,怎么协调,要根据具体的协议来决定
+    # 例如HTTP协议规定客户端必须先发请求给服务器,服务器收到后才发数据给客户端
+
+    client.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+
+    buffer = []
+    while True:  # 接收数据
+        d = client.recv(512)  # 一次最多接收指定的字节数
+        if d:
+            buffer.append(d)
+        else:
+            break
+
+    client.close()  # 关闭连接
+    data = b''.join(buffer)
+    header, html = data.split(b'\r\n\r\n', 1)
+    print(header.decode())
+    print(html.decode())
+
+
+if __name__ == "__main__":
+    tcp_client()
--- code/socket/tcp_server.py
+++ code/socket/tcp_server.py
@@ -11,19 +11,22 @@ def tcp_link(sock, addr):
         time.sleep(1)
         if not data or data == b'exit':
             break
-        sock.send(
-            'Hello, {}!'.format(data).encode('utf-8'))  # encode成bytes类型,如何需要发送多种类型的数据,可考虑用struct.pack struct.unpack
+        sock.send(f'Hello, {data}!'.encode())  # encode成bytes类型,如何需要发送多种类型的数据,可考虑用struct.pack struct.unpack
     sock.close()
     print('Connection from {}:{} closed.'.format(*addr))
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 9999))  # 监听端口,0.0.0.0表示绑定到所有的网络地址
-server.listen(5)  # 调用listen()方法开始监听端口,传入的参数指定等待连接的最大数量
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', 9999))  # 监听端口,0.0.0.0表示绑定到所有的网络地址
+    server.listen(5)  # 调用listen()方法开始监听端口,传入的参数指定等待连接的最大数量
+    while True:  # 服务器程序通过一个永久循环来接受来自客户端的连接,accept()会等待并返回一个客户端的连接
+        # 接受一个新连接:
+        sock, addr = server.accept()  # 每个连接都必须创建新线程(或进程)来处理,否则单线程在处理连接的过程中,无法接受其他客户端的连接
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcp_link, args=(sock, addr))
+        t.start()
 
-while True:  # 服务器程序通过一个永久循环来接受来自客户端的连接,accept()会等待并返回一个客户端的连接
-    # 接受一个新连接:
-    sock, addr = server.accept()  # 每个连接都必须创建新线程(或进程)来处理,否则单线程在处理连接的过程中,无法接受其他客户端的连接
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcp_link, args=(sock, addr))
-    t.start()
+
+if __name__ == "__main__":
+    main()
