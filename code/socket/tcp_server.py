0915509fff5af538413fc409b315131ef4e709da
neos <akatsuki10086@gmail.com>
2023-07-20 19:03:45 +0800

    Update tcp_server.py

--- code/socket/tcp_server.py
+++ code/socket/tcp_server.py
@@ -1,6 +1,6 @@
 import socket
-import threading
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 
 def tcp_link(sock, addr):
@@ -20,10 +20,10 @@ def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET指定使用IPv4协议,SOCK_STREAM指定使用面向流的TCP协议
     server.bind(('127.0.0.1', 9999))  # 监听端口,0.0.0.0表示绑定到所有的网络地址
     server.listen(5)  # 调用listen()方法开始监听端口,传入的参数指定等待连接的最大数量
-    while True:
-        sock, addr = server.accept()  # 等待并返回一个客户端的连接
-        t = threading.Thread(target=tcp_link, args=(sock, addr))  # 每个连接创建一个新线程,否则在处理连接过程中无法接受其他连接
-        t.start()
+    with ThreadPoolExecutor(10) as executor:  # 每个连接用线程处理,否则在处理连接过程中无法接受其他客户端连接
+        while True:
+            sock, addr = server.accept()  # 等待并返回一个客户端的连接
+            executor.submit(tcp_link, sock, addr)
 
 
 if __name__ == "__main__":
