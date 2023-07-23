85ade9d312458ed79769bb2924dadfe6b0b81764
neos <akatsuki10086@gmail.com>
2023-07-23 14:02:22 +0800

    Update redlock.py

--- code/redlock.py
+++ code/redlock.py
@@ -1,33 +1,25 @@
 import logging
 import random
-import redis
 import threading
 import time
 from hashlib import sha1
 from os import urandom
 
+import redis
 from redis.exceptions import RedisError, NoScriptError
 
-
-def get_servers(connection_list):
-    servers = []
-    for connection_info in connection_list:
-        if isinstance(connection_info, str):
-            server = redis.Redis.from_url(connection_info)
-        elif isinstance(connection_info, dict):
-            server = redis.Redis(**connection_info)
-        else:
-            server = connection_info
-        servers.append(server)
-    return servers
-
-
-def do_something(idx, room_lock, song_lock):
-    print('Im doing something in idx {}'.format(idx))
-    lock = room_lock if idx & 1 else song_lock
-    with lock:
-        time.sleep(1)
-        1 / 0
+"""
+refer: https://redis.io/docs/manual/patterns/distributed-locks/
+The Redlock Algorithm
+假设有N=5个完全独立的Redis主节点(无副本),因此需要在不同的计算机上运行5个Redis实例
+为了获取锁,客户端执行以下操作:
+1. 获取当前以毫秒为单位时间
+2. 尝试在N个实例中使用相同key-value和比总锁自动释放时间小很多的超时时间(如果某个实例不可用应该尽快尝试与下一个实例通信)来获取锁
+3. 当前时间减去步骤1中的时间戳来计算获取锁花费的时间,当且仅当客户端在大多数实例(至少3个)中获取锁,且获取锁花费时间小于锁有效期才认为获取了锁
+4. 如果获取了锁,则其有效时间为初始有效时间减去经过的时间(步骤3中的计算)
+5 如果客户端未能获取锁(要么无法锁定N/2+1个实例,要么有效期为负),它将尝试解锁所有实例(甚至是它认为无法锁定的实例)
+说明: 如果是一主一丛,主节点挂掉,但锁还未来得及同步到从节点会导致其他进程再次获取锁
+"""
 
 
 class Redlock:
@@ -46,9 +38,7 @@ class Redlock:
     def __init__(self, servers, name, ttl=10000, blocking_timeout=20):
         """
         reference:
-            https://redis.io/topics/distlock
-            https://github.com/SPSCommerce/redlock-py
-            https://github.com/andymccurdy/redis-py/blob/master/redis/lock.py
+            https://github.com/redis/redis-py/blob/master/redis/lock.py
         ttl is the number of milliseconds for the validity time.如果担心加锁业务在锁过期时还未执行完,可以给该业务加一个定时任务,定时检测业务是否还存活并决定是否给锁续期
         the token is placed in thread local storage so that a thread only sees its token, not a token set by another thread. Consider the following timeline:
         time: 0, thread-1 acquires `my-lock`, with a timeout of 5 seconds.thread-1 sets the token to "abc"
@@ -80,8 +70,7 @@ class Redlock:
 
     def lock(self):
         self.local.token = urandom(16)
-        drift = int(
-            self.ttl * .01) + 2  # Add 2 milliseconds to the drift to account for Redis expires precision which is 1 millisecond, plus 1 millisecond min drift for small TTLs.
+        drift = int(self.ttl * .01) + 2
         start_time = time.time()
         stop_at = start_time + self.blocking_timeout
         while start_time < stop_at:
@@ -98,8 +87,7 @@ class Redlock:
                 return True
             else:  # 如果锁获取失败应立马释放获取的锁定
                 self.unlock()
-                time.sleep(random.uniform(0,
-                                          .4))  # a random delay in order to try to desynchronize multiple clients trying to acquire the lock for the same resource at the same time
+                time.sleep(random.uniform(0, .4))  # 随机延迟,防止脑裂情况(split brain condition )
             start_time = time.time()
         raise Exception("lock timeout")
 
@@ -115,20 +103,25 @@ class Redlock:
         return True
 
 
-servers = get_servers([
-    {"host": "localhost", "port": 2345, "db": 0},
-    {"host": "localhost", "port": 8002, "db": 0},
-    {"host": "localhost", "port": 8003, "db": 0},
-])
+def do_something(idx, lock, another_lock):
+    # 如果do_something耗时大于锁生存周期ttl,会出现并发问题,总耗时变小
+    # 如果锁内部token未使用threading.local存储,会出现并发问题,总耗时变小
+    print('Im doing something in idx {}'.format(idx))
+    if idx & 1:
+        lock = another_lock
+    with lock:
+        time.sleep(1)
+        1 / 0
 
-'''
-如果do_something耗时大于锁生存周期ttl,会出现并发问题,总耗时变小
-如果锁内部token未使用threading.local存储,会出现并发问题,总耗时变小
-'''
-room_lock = Redlock(servers, 'room_lock', ttl=500)
-song_lock = Redlock(servers, 'song_lock', ttl=500)
 
 if __name__ == '__main__':
+    servers = [
+        redis.Redis(host="localhost", port=2345),
+        redis.Redis(host="localhost", port=6379),
+        redis.Redis(host="localhost", port=6380),
+    ]
+    room_lock = Redlock(servers, 'room_lock', ttl=500)
+    song_lock = Redlock(servers, 'song_lock', ttl=500)
     threads = [threading.Thread(target=do_something, args=(idx, room_lock, song_lock)) for idx in range(10)]
     for thread in threads:
         thread.start()
