d0fa26be5ff7d4a3889217126cfa1e204634b7c3
neos <akatsuki10086@gmail.com>
2023-08-02 16:41:12 +0800

    Update tutorial.py

--- code/tutorial.py
+++ code/tutorial.py
@@ -1128,7 +1128,7 @@ class PickleTutorial:
 
 
 def pickle_tutorial():
-    # 除个别外(如不能序列化lambda表达式), pickle.dumps可以序列化任何数据类型成b字符串,并保留原有的数据(比如生成好的树,图结构)
+    # 除个别外(如不能序列化lambda表达式,线程锁), pickle.dumps可以序列化任何数据类型成b字符串,并保留原有的数据(比如生成好的树,图结构)
     # pickle.loads反序列化后的对象与原对象是等值的副本对象, 类似与deepcopy
     # pickle模块并不安全,你只应该对你信任的数据进行unpickle操作
     with open("data", "wb") as f:
@@ -1143,7 +1143,7 @@ def pickle_tutorial():
 
 def inherit_tutorial():
     # MRO全称Method Resolution Order,用来定义继承方法的调用顺序
-    # super是一个类,第二个参数决定使用哪个类的mro,第一个参数决定从它后面第一个类开始找,而不是父类的方法(还有可能代理其兄弟类)
+    # super是一个类,第一个参数决定从它后面第一个类开始找,而不是父类的方法(还有可能代理其兄弟类),第二个参数决定使用哪个类的mro
     # 在继承中一旦定义了子类的构造函数,则需要在第一行显示调用基类的构造函数super().__init__()
     class A:  # 模拟object类
         def __init__(self):
@@ -1169,7 +1169,7 @@ def inherit_tutorial():
             print("hello C")
 
         def print(self):
-            print("print C", self, self.aa, self.__bb)
+            print("print C", self, self.aa, self.__bb)  # self.__bb会被解释称self._C__bb
 
     class D(B, C):
         aa, __bb = [2, 4]
@@ -1198,6 +1198,48 @@ def inherit_tutorial():
     B()
     # init A
     # init B
+
+
+def frame_tutorial():
+    def parent(a=1):
+        b = [2]
+        son()
+
+    def son():
+        # 每个函数编译期间会编译出一个code object, 每次调用时会产生一个新的frame
+        frame = inspect.currentframe()
+        print(frame.f_code.co_name)  # son,当前函数名
+        print(frame.f_back.f_code.co_name)  # parent,父函数名
+        print(frame.f_back.f_locals)  # {'a': 1, 'b': [2], 'son': <function son>},父函数局部变量
+        whereis = f"{frame.f_back.f_code.co_filename} at line {frame.f_back.f_lineno}"
+        print(whereis)  # 调用该函数的文件路径名 + 代码在该文件在行,方便调试,还可以递归打印二叉树路径
+
+    parent()
+
+
+def float_tutorial():
+    """
+    c语言中浮点数在内存分布如下:
+    float    符号位1比特    指数位8比特    尾数位23比特
+    double   符号位1比特    指数位11比特   尾数位52比特
+    整数转换成补码再写入内存,浮点数直接写入内存
+    浮点数被存储之前,先转换为a*(2**n)的二进制形式,其中1<=a<2,n是指数
+    浮点数的精度由尾数位长度决定,float精度为6~7位有效数字(2**23=8388608),double精度为15~16位有效数字(2**52=4503599627370496)
+    float取值范围(-2**128 ~ 2**128),double取值范围(-2**1024 ~ 2**1024),此处的128由255-127得到
+    例如float类型19.625 = 10011.101 = 1.0011101 * 2**4, 对应a=1.0011101, n=4
+    二进制小数部分(0011101)放入内存中的尾数位,当小数过长时,多出的位数会被直接截去
+    二进制整数部分都是1,无需在内存中体现出来,所以直接截去,只把指数4+127=131=0b10000011放入内存中的指数位,加127是应为指数可能为负
+    二进制符号位为正,所以把0放入内存符号部分
+    综上所述,float类型19.625在内存以0 10000011 00111010000000000000000
+    """
+    wanted_value = 0b01000001100111010000000000000000
+    test_float = ctypes.c_float(19.625)
+    assert struct.unpack("<I", test_float)[0] == wanted_value  # 说明整数在c语言中以小端模式存储
+    assert struct.unpack(">I", struct.pack(">f", test_float.value))[0] == wanted_value  # 保证pack和unpack模式顺序一致即可
+
+    wanted_value = 0b11000011000000000001100111011011
+    test_float = ctypes.c_float(-128.101)  # 对应二进制小数为(-1)*1.00000000001100111011011001000101101*2**7
+    assert struct.unpack("<I", test_float)[0] == wanted_value  # 说明多出的小数部分被截去
 
 
 def metaclass_tutorial():
@@ -1276,50 +1318,7 @@ def singleton_tutorial():
     assert TestSingleTonPool() is TestSingleTonPool()
 
 
-def frame_tutorial():
-    def parent(a=1):
-        b = [2]
-        son()
-
-    def son():
-        # 每个函数编译期间会编译出一个code object, 运行时每次调用会产生一个新的frame
-        frame = inspect.currentframe()
-        print(frame.f_code.co_name)  # son,当前函数名
-        print(frame.f_back.f_code.co_name)  # parent,父函数名
-        print(frame.f_back.f_locals)  # {'a': 1, 'b': [2], 'son': <function son>},父函数局部变量
-        whereis = f"{frame.f_back.f_code.co_filename} at line {frame.f_back.f_lineno}"
-        print(whereis)  # 调用该函数的文件路径名 + 代码在该文件在行,方便调试,还可以递归打印二叉树路径
-
-    parent()
-
-
-def float_tutorial():
-    """
-    c语言中浮点数在内存分布如下:
-    float    符号位1比特    指数位8比特    尾数位23比特
-    double   符号位1比特    指数位11比特   尾数位52比特
-    整数转换成补码再写入内存,浮点数直接写入内存
-    浮点数被存储之前,先转换为a*(2**n)的二进制形式,其中1<=a<2,n是指数
-    浮点数的精度由尾数位长度决定,float精度为6~7位有效数字(2**23=8388608),double精度为15~16位有效数字(2**52=4503599627370496)
-    float取值范围(-2**128 ~ 2**128),double取值范围(-2**1024 ~ 2**1024),此处的128由255-127得到
-    例如float类型19.625 = 10011.101 = 1.0011101 * 2**4, 对应a=1.0011101, n=4
-    二进制小数部分(0011101)放入内存中的尾数位,当小数过长时,多出的位数会被直接截去
-    二进制整数部分都是1,无需在内存中体现出来,所以直接截去,只把指数4+127=131=0b10000011放入内存中的指数位,加127是应为指数可能为负
-    二进制符号位为正,所以把0放入内存符号部分
-    综上所述,float类型19.625在内存以0 10000011 00111010000000000000000
-    """
-    wanted_value = 0b01000001100111010000000000000000
-    test_float = ctypes.c_float(19.625)
-    assert struct.unpack("<I", test_float)[0] == wanted_value  # 说明整数在c语言中以小端模式存储
-    assert struct.unpack(">I", struct.pack(">f", 19.625))[0] == wanted_value  # 保证pack和unpack模式顺序一致即可
-
-    wanted_value = 0b11000011000000000001100111011011
-    test_float = ctypes.c_float(-128.101)  # 对应二进制小数为(-1)*1.00000000001100111011011001000101101*2**7
-    assert struct.unpack("<I", test_float)[0] == wanted_value  # 说明多出的小数部分被截去
-
-
 if __name__ == "__main__":  # import到其他脚本中不会执行以下代码,spawn方式的多进程也需要
-    # float_tutorial()
     # frame_tutorial()
     # arguments_tutorial()
     # list_tutorial()
@@ -1335,4 +1334,5 @@ if __name__ == "__main__":  # import到其他脚本中不会执行以下代码,s
     # singleton_tutorial()
     # isinstance_tutorial()
     # with_tutorial()
-    variable_tutorial()
+    # variable_tutorial()
+    float_tutorial()
