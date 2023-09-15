eefdb4bf5c925805c78ccc01fe0db1dcd80185f5
neos <akatsuki10086@gmail.com>
2023-09-15 14:30:14 +0800

    Update gorutinue.go

--- golang/exercise/gorutinue.go
+++ golang/exercise/gorutinue.go
@@ -9,57 +9,36 @@ import (
 
 // 主协程退出,其他子协程也要跟着退出,Goroutine没有ID号
 
-func f1(ch1 chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 10; i++ {
-		ch1 <- i
-	}
-	close(ch1) // 用完关闭,防止f2死锁,通道关闭后可以取到默认值,且第二个参数是false
-}
-
-func f2(ch1 <-chan int, ch2 chan<- int, wg *sync.WaitGroup) { // 单向通道,限定通道方向
-	defer wg.Done()
-	for i := range ch1 {
-		ch2 <- i * i
-	}
-	close(ch2)
-}
-
-var counter = 0
-
-func TestRaceCondition(lock *sync.Mutex, wg *sync.WaitGroup) {
-	lock.Lock()
-	defer lock.Unlock()
-	defer wg.Done() // 等价于wg.Add(-1)
-	counter++
-}
-
-func main() {
-	wg := sync.WaitGroup{}   // WaitGroup传递要使用指针
-	lock := sync.Mutex{}     // 互斥锁,传递要使用指针
-	rwLock := sync.RWMutex{} // 读写锁,传递要使用指针
-
-	mapping := sync.Map{} // 该map并发安全,读写通过Load和Store实现,key-value类型可以不同
-	mapping.Store("key", "value")
-	mapping.Store(1, 'A')
-
+func testWaitGroup() {
+	wg := sync.WaitGroup{}    // WaitGroup传递要使用指针
 	ch1 := make(chan int, 5)  // make返回的都是引用类型
 	ch2 := make(chan int, 10) // 定义channel的时候也可以定义单向通道,双向通道可以作为单向通道的入参
+
 	wg.Add(2)
-	go f1(ch1, &wg)
-	go f2(ch1, ch2, &wg)
+	go func(ch1 chan<- int, wg *sync.WaitGroup) {
+		defer wg.Done()
+		//runtime.Goexit()  // 退出当前协程
+		for i := 0; i < 10; i++ {
+			ch1 <- i
+		}
+		close(ch1) // 用完关闭,防止f2死锁,通道关闭后可以取到默认值,且第二个参数是false
+	}(ch1, &wg)
+	go func(ch1 <-chan int, ch2 chan<- int, wg *sync.WaitGroup) { // 单向通道,限定通道方向
+		defer wg.Done()
+		for i := range ch1 {
+			ch2 <- i * i
+		}
+		close(ch2)
+	}(ch1, ch2, &wg)
 	wg.Wait()
+
 	for ret := range ch2 {
 		fmt.Println(ret)
 	}
+}
 
-	for i := 0; i < 50000; i++ {
-		wg.Add(1) // 一定要在Wait函数执行前执行
-		go TestRaceCondition(&lock, &wg)
-	}
-	wg.Wait() // 会阻塞代码的运行,直到计数器值减为0
-	fmt.Println("counter的值是 ", counter)
-
+func testMutex() {
+	rwLock := sync.RWMutex{} // 读写锁,传递要使用指针
 	rwLock.Lock()
 	for i := 0; i < 4; i++ {
 		go func(i int) {
@@ -77,6 +56,33 @@ func main() {
 	rwLock.Lock() // 会等读锁全部释放,才能获得写锁
 	fmt.Println("程序退出...")
 	rwLock.Unlock()
+}
+
+func testRaceCondition() {
+	counter := 0
+	lock := sync.Mutex{} // 互斥锁,传递要使用指针
+	wg := sync.WaitGroup{}
+	for i := 0; i < 50000; i++ {
+		wg.Add(1) // 一定要在Wait函数执行前执行
+		go func(lock *sync.Mutex, wg *sync.WaitGroup) {
+			lock.Lock()
+			defer lock.Unlock()
+			defer wg.Done() // 等价于wg.Add(-1)
+			counter++
+		}(&lock, &wg)
+	}
+	wg.Wait() // 会阻塞代码的运行,直到计数器值减为0
+	fmt.Println("counter的值是", counter)
+}
+
+func main() {
+	//testWaitGroup()
+	testMutex()
+	//testRaceCondition()
+
+	mapping := sync.Map{} // 该map并发安全,读写通过Load和Store实现,key-value类型可以不同
+	mapping.Store("key", "value")
+	mapping.Store(1, 'A')
 
 	// 信道当WaitGroup使用
 	done := make(chan bool)
@@ -89,6 +95,7 @@ func main() {
 	<-done
 
 	// 信道当锁使用
+	wg := sync.WaitGroup{}
 	chanLock := make(chan bool, 1)
 	var x int
 	for i := 0; i < 1000; i++ {
