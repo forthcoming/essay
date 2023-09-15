d32d829c6b38f0adbb500e3fec9d2357ee369211
neos <akatsuki10086@gmail.com>
2023-09-15 23:14:51 +0800

    update

--- golang/gorutinue.go
+++ golang/gorutinue.go
@@ -10,18 +10,21 @@ import (
 // 主协程退出,其他子协程也要跟着退出,Goroutine没有ID号
 
 func testWaitGroup() {
-	wg := sync.WaitGroup{}    // WaitGroup传递要使用指针
-	ch1 := make(chan int, 5)  // make返回的都是引用类型
-	ch2 := make(chan int, 10) // 定义channel的时候也可以定义单向通道,双向通道可以作为单向通道的入参
+	wg := sync.WaitGroup{} // WaitGroup传递要使用指针
+	// 方向只有<-,表示数据从右向左传输
+	ch1 := make(chan int, 5)
+	// 带有缓冲的channel,缓冲区满时插入数据会阻塞,缓冲区空时获取数据会阻塞
+	ch2 := make(chan int, 10)
+	fmt.Println("len:", len(ch2), "cap:", cap(ch2)) // len: 0 cap: 10
 
 	wg.Add(2)
-	go func(ch1 chan<- int, wg *sync.WaitGroup) {
+	go func(ch1 chan<- int, wg *sync.WaitGroup) { // 可以定义单向通道的channel,双向通道可以作为单向通道的入参
 		defer wg.Done()
-		//runtime.Goexit()  // 退出当前协程
-		for i := 0; i < 10; i++ {
+		//runtime.Goexit() // 退出当前协程
+		for i := 0; i < 5; i++ {
 			ch1 <- i
 		}
-		close(ch1) // 用完关闭,防止f2死锁,通道关闭后可以取到默认值,且第二个参数是false
+		close(ch1) // 用完关闭,防止下面协程死锁,通道关闭后可以取到默认值,且第二个参数是false,无法向关闭的通道中发送数据
 	}(ch1, &wg)
 	go func(ch1 <-chan int, ch2 chan<- int, wg *sync.WaitGroup) { // 单向通道,限定通道方向
 		defer wg.Done()
@@ -35,6 +38,57 @@ func testWaitGroup() {
 	for ret := range ch2 {
 		fmt.Println(ret)
 	}
+	//for {
+	//	if ret, ok := <-ch2; ok {
+	//		fmt.Println(ret)
+	//	} else {
+	//		break
+	//	}
+	//}
+}
+
+func testRaceCondition() {
+	counter := 0
+	lock := sync.Mutex{} // 互斥锁,传递要使用指针
+	wg := sync.WaitGroup{}
+	for i := 0; i < 50000; i++ {
+		wg.Add(1) // 一定要在Wait函数执行前执行
+		go func(lock *sync.Mutex, wg *sync.WaitGroup) {
+			lock.Lock()
+			defer lock.Unlock()
+			defer wg.Done() // 等价于wg.Add(-1)
+			counter++
+		}(&lock, &wg)
+	}
+	wg.Wait() // 会阻塞代码的运行,直到计数器值减为0
+	fmt.Println("counter的值是", counter)
+}
+
+func testSelect() {
+	ch := make(chan int)
+	stop := make(chan bool)                           // 不带缓存的channel
+	fmt.Println("len:", len(stop), "cap:", cap(stop)) // len: 0 cap: 0
+	go func() {
+		for j := 0; j < 7; j++ {
+			ch <- j
+			time.Sleep(time.Second)
+		}
+		//close(ch)  // 注意这里不要close,应为close一个channel也可以使select语句不再阻塞,for中的channel要close
+		time.Sleep(time.Second)
+		stop <- true
+	}()
+	for { // 死循环
+		select { // 同时处理多个channel
+		//case ch <- 1: // 如果成功向ch写入数据,则执行对应case处理语句
+		case c := <-ch:
+			fmt.Println("Receive", c)
+		case _ = <-stop:
+			goto ENDLOOP // 此处不能用break,应为break仅仅退出select,也可以用return
+		case <-time.After(10 * time.Second):
+			fmt.Println("timeout!")
+		}
+	}
+ENDLOOP:
 }
 
 func testMutex() {
@@ -58,27 +112,12 @@ func testMutex() {
 	rwLock.Unlock()
 }
 
-func testRaceCondition() {
-	counter := 0
-	lock := sync.Mutex{} // 互斥锁,传递要使用指针
-	wg := sync.WaitGroup{}
-	for i := 0; i < 50000; i++ {
-		wg.Add(1) // 一定要在Wait函数执行前执行
-		go func(lock *sync.Mutex, wg *sync.WaitGroup) {
-			lock.Lock()
-			defer lock.Unlock()
-			defer wg.Done() // 等价于wg.Add(-1)
-			counter++
-		}(&lock, &wg)
-	}
-	wg.Wait() // 会阻塞代码的运行,直到计数器值减为0
-	fmt.Println("counter的值是", counter)
-}
-
 func main() {
 	//testWaitGroup()
-	testMutex()
 	//testRaceCondition()
+	testSelect()
+	//testMutex()
+	return
 
 	mapping := sync.Map{} // 该map并发安全,读写通过Load和Store实现,key-value类型可以不同
 	mapping.Store("key", "value")
@@ -129,29 +168,6 @@ func main() {
 		case channel <- i:
 		}
 	}
-
-	ch := make(chan int)
-	stop := make(chan bool)
-	go func() {
-		for j := 0; j < 7; j++ {
-			ch <- j
-			time.Sleep(time.Second)
-		}
-		//close(ch)  // 注意这里不要close,应为close一个channel也可以使select语句不再阻塞,for中的channel要close
-		time.Sleep(time.Second)
-		stop <- true
-	}()
-	for { // 死循环
-		select {
-		case c := <-ch:
-			fmt.Println("Receive", c)
-		case _ = <-stop:
-			goto ENDLOOP // 此处不能用break,应为break仅仅退出select
-		case <-time.After(10 * time.Second):
-			fmt.Println("timeout!")
-		}
-	}
-ENDLOOP:
 }
 
 func sendRegisterEmailAsync(destination, here string) {
@@ -163,7 +179,7 @@ func sendRegisterEmailAsync(destination, here string) {
 			fmt.Printf("in sendRegisterEmailAsync err=%v\n", err)
 		}
 	}() // 必须, 否则协程报错会使主进程终止
-	//data := map[string]interface{}{
+	//data := map[string]any{
 	//	"here":  here,
 	//	"email": destination,
 	//}
