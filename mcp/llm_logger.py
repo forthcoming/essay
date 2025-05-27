bc84b38d2dcbf6ac50620d62ed7afcc68fdacda1
zgt <akatsuki10086@gmail.com>
2025-05-27 19:05:51 +0800

    Update llm_logger.py

--- mcp/llm_logger.py
+++ mcp/llm_logger.py
@@ -38,3 +38,18 @@ if __name__ == "__main__":
     # nginx也可以做类似的转发操作,但没法详细的记录日志,无法很好的支持流式响应转发
     # cline配置:
     # 打开设置,API Provide选择OpenAI Compatible, Base URL填写http://localhost:8000,API Key填写OpenRouter的key,就可以使用了
+
+    # cline在传递工具时,是把工具的描述,通过messages传给了模型
+    # 如果模型要使用工具,提示词会让模型以特定的格式作为内容返回
+    # 所以就可以避免各种模型不兼容的问题
+    # 使用get_flight_info获取航班信息,模型就返回一下内容
+    # <use_mcp_tool>
+    # <server_name>local/tour</server_name>
+    # <tool_name>get_flight_info</tool_name>
+    # <arguments>
+    # {
+    #   "date": "2025-04-01",
+    #   "origin": "深圳"
+    # }
+    # </arguments>
+    # </use_mcp_tool>
